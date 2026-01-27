57a6dee079da4e2d3d1a4e23a5d91dbe34b44a6d
ModMonster <modmonster2060@gmail.com>
2026-01-27 12:30:09 -0500

    Reimplement building too high system which was weirdly half-removed

--- src/main/java/ca/modmonster/spells/events/OnBlockPlace.java
+++ src/main/java/ca/modmonster/spells/events/OnBlockPlace.java
@@ -1,6 +1,7 @@
 package ca.modmonster.spells.events;
 
 import ca.modmonster.spells.game.GameManager;
+import ca.modmonster.spells.util.Utilities;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -9,6 +10,11 @@ public class OnBlockPlace implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         if (event.getPlayer().hasPermission("magicwars.breakblocks") && GameManager.allowedBuildingPlayers.contains(event.getPlayer())) return;
-        event.setCancelled(true);
+
+        // prevent building too high
+        if (event.getBlockPlaced().getY() > GameManager.activeGame.world.map.maxBuildHeight) {
+            event.setCancelled(true);
+            event.getPlayer().sendActionBar(Utilities.stringToComponent("&cYou can't build this high!"));
+        }
     }
 }
